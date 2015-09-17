6a56bebd6df65c983324293c79cc048f6b1b02aa
Amol <amolnayak311@gmail.com>
2015-09-17 18:20:53 +0530

    Some changes to Search engine

--- src/ch4/searchengine.py
+++ src/ch4/searchengine.py
@@ -8,6 +8,8 @@ from pysqlite2 import dbapi2 as sqlite
 import re
 
 
+ignorewords=frozenset(['the','of','to','and','a','in','is','it'])
+
 class crawler:
     def __init__(self, dbname):
         self.con = sqlite.connect(dbname)
@@ -19,18 +21,42 @@ class crawler:
         self.con.commit()
     
     
+    #
+    #
+    #
     def getentryid(self, table, field, value, createnew=True):
-        return None
+        res = self.con.execute("select rowid from %s where %s = '%s'" % (table, field, value)).fetchone()
+        if res == None:
+            return self.con.execute("insert into %s (%s) values ('%s')" % (table, field, value)).lastrowid
+        else:
+            return res[0]
+        
     
     
     
     def addtoindex(self, url, soup):
-        if self.isindexed(url): return
+        if self.isindexed(url):
+            print "%s already indexed, skipping" % url 
+            return
 
         print "Indexing %s"%url
         
         text = self.gettextonly(soup)
-        words = self.separatewords(text)        
+        words = self.separatewords(text)
+        
+        #Create and get the id from the DB for this URL 
+        urlid = self.getentryid('urllist', 'url', url)
+        
+        for i in range(len(words)):
+            word = words[i]
+            if word in ignorewords: continue
+            
+            wordid = self.getentryid('wordlist', 'word', word)
+            self.con.execute('insert into wordlocation(urlid, wordid, location) \
+            values (%d, %d, %d)' % (urlid, wordid, i))
+    
+    
+            
         
     
     #
@@ -59,9 +85,18 @@ class crawler:
         return [w.lower() for w in splitter.split(text) if w != '']
     
     
+    #
+    #
+    #
     def isindexed(self, url):
-        return False
-    
+        val = self.con.execute("select rowid from urllist where url = '%s'" %url).fetchone()
+        #If the page is indexed is it really crawled and words indexed?
+        if val != None:
+            wordcount = self.con.execute("select count(1) from urllist where url = '%s'" % url).fetchone()
+            return False if wordcount[0] == 0 else True
+        else:
+            return False
+
     
     def addlinkref(self, urlFrom, urlTo, linkText):
         pass        
@@ -74,8 +109,8 @@ class crawler:
             for page in pages:
                 try:
                     c = urlopen(page)
-                except:
-                    print "Exception while opening page %s"%page
+                except Exception, err:
+                    print "Exception while opening page %s"%page, err
                     continue
                 
                 soup = BeautifulSoup(c.read())
@@ -99,20 +134,16 @@ class crawler:
                 self.dbcommit()        
             pages = newpages
         
-                            
-                            
-                    
-                
-                
 
     def createindextable(self, drop_existing = False):
+        #TODO: Add logic to check if tables exist and accordingly create/drop first
         if drop_existing:
             self.con.execute('drop table if exists urllist')
             self.con.execute('drop table if exists wordlist')
             self.con.execute('drop table if exists wordlocation')
             self.con.execute('drop table if exists link')
-            self.con.execute('drop table if exists linkwords')
-            
+            self.con.execute('drop table if exists linkwords')        
+        
         self.con.execute('create table urllist(url)')
         self.con.execute('create table wordlist(word)')
         self.con.execute('create table wordlocation(urlid, wordid, location)')
@@ -126,10 +157,45 @@ class crawler:
         print "Schema Successfully (Re)created"
         
     
+class searcher:
     
+    #
+    #
+    #
+    def __init__(self, dbname):
+        self.con=sqlite.connect(dbname)
+        
+    
+    #
+    #
+    #
+    def __del__(self):
+        self.con.close()
+
+    #
+    # Gets the count of documents in which the given word is present
+    #
+    def get_doc_count_for_word(self, word):
+        return self.con.execute("select count(*) from wordlist where word = '%s'" % word.lower()).fetchone()[0]
+    #
+    #
+    #
+    def getmatchrows(self, q):
+        #Split the words by space
+        words = q.split(' ')        
+        doc_count = [self.get_doc_count_for_word(word) for word in words]
+        non_zero_words = filter(lambda (_, count): count >0 , zip(words, doc_count))
+        print non_zero_words
+        #TODO
+        
+        
+
+
 
 crawler = crawler("searchindex.db")
 #crawler.createindextable(True)
 #crawler.createindextable()
-crawler.crawl(["https://en.wikipedia.org/wiki/Programming_language"])
-    
+#crawler.crawl(["https://en.wikipedia.org/wiki/Programming_language"])
+#crawler.crawl(["https://en.wikipedia.org/wiki/Functional_programming"])
+searcher = searcher('searchindex.db')
+searcher.getmatchrows("Programming in Scala and Python")
